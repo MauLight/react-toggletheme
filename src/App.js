7cef99cb12c3a2ca5d5ace28f67620b6aebb8e90
MauLight <maulisseluz@gmail.com>
2023-03-24 18:41:21 -0300

    css fixes and transitions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useState } from "react";
 import Main from "./components/main";
 import Navbar from "./components/navbar";
 import { ThemeContext } from "./context/ThemeContext";
@@ -7,16 +7,12 @@ import { ThemeContext } from "./context/ThemeContext";
 export default function App() {
 
   const [isDark, setIsDark] = useState(true);
-  const [themeMode, setThemeMode] = useState();
-
-
-  console.log(isDark);
 
   return (
     <ThemeContext.Provider value={isDark}>
       <div className="container">
         <Navbar />
-        <button className="btn border rounded-0" onClick={() => setIsDark(!isDark)} >Toggle Theme</button>
+        <button className="btn rounded-0 toggle-btn" onClick={() => setIsDark(!isDark)} >Theme</button>
         <Main />
       </div>
     </ThemeContext.Provider>
