bebc8b65d13829d73b9de4aef5c6aa918c46daf2
MauLight <maulisseluz@gmail.com>
2023-03-24 17:54:36 -0300

    darkmode - light mode enabled

---
+++ src/components/main.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react"
+import { ThemeContext } from "../context/ThemeContext";
+
+export default function Main() {
+
+    const isDark = useContext(ThemeContext);
+
+    const theme = isDark ? { 'backgroundColor': '#282c34' } : { 'backgroundColor': 'white', 'color': '#282c34' };
+
+    return (
+        <main style={theme}>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react"
+import { ThemeContext } from "../context/ThemeContext"
+import image from '../images/react-icon-small.png'
+
+export default function Navbar() {
+
+    const isDark = useContext(ThemeContext);
+
+    const theme = isDark ? { 'backgroundColor': '#282c34' } : { 'backgroundColor': 'white', 'color': '#282c34' };
+
+
+    return (
+        <nav style={theme}>
+            <img src={image} className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title" style={theme}>React Course - Project 1</h4>
+        </nav>
+    )
+};
