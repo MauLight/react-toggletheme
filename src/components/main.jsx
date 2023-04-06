37e44d49bd79453e1b3b6d112ca2b367c8f50706
MauLight <maulisseluz@gmail.com>
2023-04-06 18:43:08 -0400

    minor css changes

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,9 +7,15 @@ export default function Main() {
 
     const theme = isDark ? { 'backgroundColor': '#282c34' } : { 'backgroundColor': 'white', 'color': '#282c34' };
 
+    const item = {
+        name: "Ice cream",
+        cal: 200
+    }
+
     return (
         <main style={theme}>
             <h1 className="main--title">Fun facts about React</h1>
+            <li>{`${item.name} - ${item.cal} cal`}</li>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
